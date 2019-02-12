aa3475cdc971ad627d972dc518af08d1d3c7004d
minami.terada <s2jvrnp10vc@i.softbank.jp>
2019-02-12 15:17:50 +0000

    user login

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190211150443) do
+ActiveRecord::Schema.define(version: 20190212084927) do
 
   create_table "microposts", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string   "content"
@@ -25,6 +25,8 @@ ActiveRecord::Schema.define(version: 20190211150443) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string   "status"
+    t.integer  "user_id"
+    t.index ["user_id"], name: "index_tasks_on_user_id", using: :btree
   end
 
   create_table "users", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
@@ -36,4 +38,5 @@ ActiveRecord::Schema.define(version: 20190211150443) do
   end
 
   add_foreign_key "microposts", "users"
+  add_foreign_key "tasks", "users"
 end
